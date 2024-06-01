c799f00cfd20c1129bbd2839988af59c0102b828
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-06-01 12:40:21 +0200

    Add 100 movies

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,26 @@
 export type UserType = {
-    email: string;
-    password:string
-}
+  email: string;
+  password: string;
+};
 
 export type InputFieldProps = {
   type: string;
   placeholder: string;
   changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export type SingleMovieType = {
+  rank: number;
+  title: string;
+  thumbnail: string;
+  rating: string;
+  id: string;
+  year: number;
+  image: string;
+  description: string;
+  trailer: string;
+  genre: string[];
+  director: string[];
+  writers: string[];
+  imdbid: string;
 };
