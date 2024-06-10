624edf945452f06b3a214a26bd8af9d891c5f06d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-06-10 21:32:58 +0200

    Add overflow hidden

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,15 @@
-import MovieCard from "./MovieCard"
+import MovieCard from './MovieCard'
+
 const Slider = ({ movies }) => {
   return (
-    <div className="border w-[90%] mx-auto py-12 flex">
-      {movies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
+    <div className="border w-[90%] mx-auto flex py-12 overflow-hidden">
+      {movies.map((movie) => (
+        <div key={movie.id} className="w-1/3">
+          <MovieCard {...movie} />
+        </div>
+      ))}
     </div>
   )
 }
 
 export default Slider
-
