dd98c9da9067b1634375561a817b7f2b3641b1f8
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-06-11 21:53:29 +0200

    Add slider functionality

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,23 +1,15 @@
+
 import MovieCard from './MovieCard'
 import { HiArrowCircleLeft, HiArrowCircleRight } from 'react-icons/hi'
 import { useState } from 'react'
-const Slider = ({ movies }) => {
-  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0)
 
-  const generateDivs = (length: number) => {
-    const numDivs = Math.ceil(length / 4)
-    const divsArray = []
-    for (let i = 0; i < numDivs; i++) {
-      divsArray.push(
-        <div key={i} className="h-[10px] w-[20px] bg-gray-100"></div>,
-      )
-    }
-    return divsArray
-  }
-  const divsArray = generateDivs(movies.length)
+const Slider = ({ movies }) => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0)
+
+  const totalSlides = Math.ceil(movies.length / 4)
 
   const nextSlide = () => {
-    if (activeSlideIndex < divsArray.length - 1) {
+    if (activeSlideIndex < totalSlides - 1) {
       setActiveSlideIndex((prev) => prev + 1)
     } else {
       setActiveSlideIndex(0)
@@ -26,56 +18,50 @@ const Slider = ({ movies }) => {
 
   const previousSlide = () => {
     if (activeSlideIndex === 0) {
-      setActiveSlideIndex(divsArray.length - 1)
+      setActiveSlideIndex(totalSlides - 1)
     } else {
       setActiveSlideIndex((prev) => prev - 1)
     }
   }
 
   return (
-    <div className="w-[100%] pt-12 pb-20 flex flex-col gap-4 relative">
+    <div className="w-full pt-12 pb-20 flex flex-col gap-4 relative">
       <div className="w-[90%] mx-auto flex items-center justify-between">
         <h2>Currently Trending</h2>
         <div className="flex gap-1 items-center">
-          {divsArray.map((div, index) => {
+          {[...Array(totalSlides)].map((_, index) => {
             const color =
               activeSlideIndex === index ? 'bg-orange-500' : 'bg-gray-100'
             return (
               <div
-                key={div.key}
+                key={index}
                 className={`h-[10px] w-[20px] ${color} rounded-sm transition-all duration-200`}
               ></div>
             )
           })}
         </div>
       </div>
-      <div className="flex w-[90%] mx-auto border overflow-hidden">
+      <div className="w-[90%] mx-auto flex items-center relative">
         <HiArrowCircleLeft
           size={60}
-          className="absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 cursor-pointer transition-all duration-200 hover:text-orange-500 active:text-red-500 active:scale-90"
+          className="absolute left-0 -translate-x-full cursor-pointer transition-all duration-200 hover:text-orange-500 active:text-red-500 active:scale-90 z-10"
           onClick={previousSlide}
         />
-        <div className="relative h-[450px] w-full">
-          {movies.map((movie, index) => {
-            const calculation = 100 * (index - activeSlideIndex)
-            const translate =
-              calculation > 0
-                ? `translateX(${calculation}%)`
-                : `translateX(-${Math.abs(calculation)}%)`
-            return (
-              <div
-                key={movie.id}
-                className="absolute top-0 transition-all duration-200 w-1/4"
-                style={{ transform: translate }}
-              >
+        <div className="relative h-[450px] w-full overflow-hidden">
+          <div
+            className="relative h-full w-full flex transition-transform duration-500"
+            style={{ transform: `translateX(-${activeSlideIndex * 100}%)` }}
+          >
+            {movies.map((movie) => (
+              <div key={movie.id} className="w-1/4 flex-shrink-0">
                 <MovieCard {...movie} />
               </div>
-            )
-          })}
+            ))}
+          </div>
         </div>
         <HiArrowCircleRight
           size={60}
-          className="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2 cursor-pointer transition-all duration-200 hover:text-orange-500 active:text-red-500 active:scale-90"
+          className="absolute right-0 cursor-pointer translate-x-full transition-all duration-200 hover:text-orange-500 active:text-red-500 active:scale-90 z-10"
           onClick={nextSlide}
         />
       </div>
